56900b0b0f80d382616efbc88bde61db1aab6032
GlobalArsalan <ArsalanNasrifar@gmail.com>
2024-11-12 18:50:03 -0500

    Added the 3rd function of the project

--- project_4.cpp
+++ project_4.cpp
@@ -4,9 +4,12 @@
 /////////////////////////////////////////Main Functions
 std::size_t length(char const *a); // returns length of C-style string
 int compare(char const *str1, char const *str2); // compares whether two strings are equal or one comes before or after the other
-
+void assign(char *str1, char const *str2);//overwrites str1 with the values of str2 so they are equal after the operation
 ////////////////////////////////////////Helper Functions
 int beforeOrAfter(char const *str1, std::size_t k1, char const *char2, std::size_t k2);
+void setToNull(char *str1);
+//////////////////////////////////The main Function
+int main();
 /////////////////////////////////////////////////Main Project
 std::size_t length(char const *a) {
     int count = 0;
@@ -36,6 +39,12 @@ int compare(char const *str1, char const *str2) {
     return beforeOrAfter(str1, k1, str2, k2);
     
 }
+void assign(char *str1, char const *str2) {
+    setToNull(str1);
+    for(int i = 0; i <21; i++) {//Given that str1 and str2 will have size 21
+        str1[i] = str2[i];
+    }
+}
 //////////////////////////////////////Helper Functions///////////////////////////////////////////
 int beforeOrAfter(char const *str1, std::size_t k1, char const *str2, std::size_t k2) { //determines whether str1 comes before or after str2
     char character1 = str1[k1];
@@ -60,4 +69,19 @@ int beforeOrAfter(char const *str1, std::size_t k1, char const *str2, std::size_
         return 1;
     }
 
+}
+void setToNull(char *str1) { //Sets all elements in the string to \0
+    for(int i = 0; i < 21; i++) { //Given that size of str1 is 21 elements
+        str1[i] = '\0';
+    }
+}
+////////////////////////////////////The main Function////////////////////////////////////////////////
+int main() {
+    char *str1 = new char[21]{'a', '6', '\0'};
+    char const *str2 = new char[21]{'a', 'r', 's', 'a', 'l', 'a', 'b','n', 'n', '\0'};
+    assign(str1, str2);
+    for(int i = 0; i <21; i++) {
+        std::cout << str1[i];
+    }
+    return 0;
 }
