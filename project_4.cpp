2c837bc64695e694d339a0314928f501689d7187
GlobalArsalan <ArsalanNasrifar@gmail.com>
2024-11-13 20:45:55 -0500

    Added functions 4 and 5 but 4 is still a WIP

--- project_4.cpp
+++ project_4.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+#include <algorithm>
 
 /////////////////////////////////////////Main Functions
 std::size_t length(char const *a); // returns length of C-style string
 int compare(char const *str1, char const *str2); // compares whether two strings are equal or one comes before or after the other
 void assign(char *str1, char const *str2);//overwrites str1 with the values of str2 so they are equal after the operation
+unsigned int distance(char const *str1, char const *str2);//returns the minimum numebr of changes needed to turn one string to the other.
+std::size_t is_sorted(char *array[], std::size_t capacity); //check if an array of strings is sorted
 ////////////////////////////////////////Helper Functions
 int beforeOrAfter(char const *str1, std::size_t k1, char const *char2, std::size_t k2);
 void setToNull(char *str1);
@@ -45,6 +47,34 @@ void assign(char *str1, char const *str2) {
         str1[i] = str2[i];
     }
 }
+unsigned int distance(char const *str1, char const *str2) {
+    if(length(str1) == 0 || length(str2) == 0) {
+        if(length(str1) != 0) { //check which one is the empty string
+            return length(str1);
+        } else {
+            return length(str2);
+        }
+    }
+    if(str1[0] == str2[0]) { //check to see if the first character in both are equal
+        return distance(str1 + 1, str2 + 1);
+    }
+    /////////////////////////////////////////////////////////error starts here
+    //if((str1[0] != str2[0]) && (str1[1] != str2[1])) { //check to see if the first two characters are different
+        unsigned int temp1 = distance(str1 + 1, str2 + 1);//Note : check the condition, what if the string is too short?
+        unsigned int temp2 = distance(str1, str2 + 1);
+        unsigned int temp3 = distance(str1 + 1, str2);
+        return (std::min(std::min(temp1, temp2), temp3));
+    // }
+    //     return (1 + distance(str1 + 1, str2 + 1)); //case where only the first letters are different
+}
+std::size_t is_sorted(char *array[], std::size_t capacity) {
+    for(int i = 0; i < capacity - 1; i++) {
+        if(compare(array[i], array[i+1]) > 0) {
+            return i+1;
+        }
+    }
+    return capacity;
+}
 //////////////////////////////////////Helper Functions///////////////////////////////////////////
 int beforeOrAfter(char const *str1, std::size_t k1, char const *str2, std::size_t k2) { //determines whether str1 comes before or after str2
     char character1 = str1[k1];
@@ -77,11 +107,7 @@ void setToNull(char *str1) { //Sets all elements in the string to \0
 }
 ////////////////////////////////////The main Function////////////////////////////////////////////////
 int main() {
-    char *str1 = new char[21]{'a', '6', '\0'};
-    char const *str2 = new char[21]{'a', 'r', 's', 'a', 'l', 'a', 'b','n', 'n', '\0'};
-    assign(str1, str2);
-    for(int i = 0; i <21; i++) {
-        std::cout << str1[i];
-    }
+    char *array[4] = {"abc", "def", "ghi", "ekl"};
+    std::cout << is_sorted(array, 4) << std::endl;
     return 0;
 }
